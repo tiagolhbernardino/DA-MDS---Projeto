f43ca1a2a4097809fa884664883f47b56ec13aea
TiagoEstre <145720543+TiagoEstre@users.noreply.github.com>
2024-05-10 21:11:30 +0100

    Add files via upload
    Create Class

---
+++ PROJETO/iCantina/iCantina/iCantina/models/Reserva.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iCantina.models
+{
+    internal class Reserva
+    {
+        // Class
+        public Cliente Cliente {  get; set; }
+        public Multa Multa { get; set; }
+        public Prato Prato { get; set; }
+        public Menu Menu { get; set; }
+        public Extra Extra { get; set; }
+    }
+}
