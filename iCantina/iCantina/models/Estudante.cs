e8e0c21b93934829512c660e3bd0a7e51244fb3f
TiagoEstre <145720543+TiagoEstre@users.noreply.github.com>
2024-05-17 21:03:20 +0100

    Add files via upload

---
+++ iCantina/iCantina/models/Estudante.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iCantina.models
+{
+    internal class Estudante
+    {
+        public int numeroEstudante {  get; set; }
+
+
+    }
+}
