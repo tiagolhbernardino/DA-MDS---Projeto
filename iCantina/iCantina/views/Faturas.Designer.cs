68a85fc541aed7828f039c03331b9fe57e2e5c8b
TiagoBernardino <2231448@my.ipleiria.pt>
2024-05-19 20:14:31 +0100

    Layout faturas

--- iCantina/iCantina/views/Faturas.Designer.cs
+++ iCantina/iCantina/views/Faturas.Designer.cs
@@ -29,29 +29,106 @@
         private void InitializeComponent()
         {
             this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.labelEstado = new System.Windows.Forms.Label();
+            this.labelDia = new System.Windows.Forms.Label();
+            this.labelEmenta = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.BottomToolStripPanel = new System.Windows.Forms.ToolStripPanel();
+            this.TopToolStripPanel = new System.Windows.Forms.ToolStripPanel();
+            this.RightToolStripPanel = new System.Windows.Forms.ToolStripPanel();
+            this.LeftToolStripPanel = new System.Windows.Forms.ToolStripPanel();
+            this.ContentPanel = new System.Windows.Forms.ToolStripContentPanel();
             this.SuspendLayout();
             // 
             // monthCalendar1
             // 
-            this.monthCalendar1.Location = new System.Drawing.Point(78, 218);
+            this.monthCalendar1.Location = new System.Drawing.Point(29, 30);
             this.monthCalendar1.Name = "monthCalendar1";
             this.monthCalendar1.TabIndex = 0;
             // 
+            // labelEstado
+            // 
+            this.labelEstado.AutoSize = true;
+            this.labelEstado.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelEstado.Location = new System.Drawing.Point(25, 376);
+            this.labelEstado.Name = "labelEstado";
+            this.labelEstado.Size = new System.Drawing.Size(84, 25);
+            this.labelEstado.TabIndex = 1;
+            this.labelEstado.Text = "Estado: ";
+            // 
+            // labelDia
+            // 
+            this.labelDia.AutoSize = true;
+            this.labelDia.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDia.Location = new System.Drawing.Point(57, 325);
+            this.labelDia.Name = "labelDia";
+            this.labelDia.Size = new System.Drawing.Size(52, 25);
+            this.labelDia.TabIndex = 2;
+            this.labelDia.Text = "Dia: ";
+            // 
+            // labelEmenta
+            // 
+            this.labelEmenta.AutoSize = true;
+            this.labelEmenta.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelEmenta.Location = new System.Drawing.Point(24, 429);
+            this.labelEmenta.Name = "labelEmenta";
+            this.labelEmenta.Size = new System.Drawing.Size(85, 25);
+            this.labelEmenta.TabIndex = 4;
+            this.labelEmenta.Text = "Ementa:";
+            // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(717, 30);
+            this.textBox1.Location = new System.Drawing.Point(388, 30);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(399, 253);
-            this.textBox1.TabIndex = 1;
+            this.textBox1.Size = new System.Drawing.Size(401, 577);
+            this.textBox1.TabIndex = 5;
+            // 
+            // BottomToolStripPanel
+            // 
+            this.BottomToolStripPanel.Location = new System.Drawing.Point(0, 0);
+            this.BottomToolStripPanel.Name = "BottomToolStripPanel";
+            this.BottomToolStripPanel.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            this.BottomToolStripPanel.RowMargin = new System.Windows.Forms.Padding(4, 0, 0, 0);
+            this.BottomToolStripPanel.Size = new System.Drawing.Size(0, 0);
+            // 
+            // TopToolStripPanel
+            // 
+            this.TopToolStripPanel.Location = new System.Drawing.Point(0, 0);
+            this.TopToolStripPanel.Name = "TopToolStripPanel";
+            this.TopToolStripPanel.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            this.TopToolStripPanel.RowMargin = new System.Windows.Forms.Padding(4, 0, 0, 0);
+            this.TopToolStripPanel.Size = new System.Drawing.Size(0, 0);
+            // 
+            // RightToolStripPanel
+            // 
+            this.RightToolStripPanel.Location = new System.Drawing.Point(0, 0);
+            this.RightToolStripPanel.Name = "RightToolStripPanel";
+            this.RightToolStripPanel.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            this.RightToolStripPanel.RowMargin = new System.Windows.Forms.Padding(4, 0, 0, 0);
+            this.RightToolStripPanel.Size = new System.Drawing.Size(0, 0);
+            // 
+            // LeftToolStripPanel
+            // 
+            this.LeftToolStripPanel.Location = new System.Drawing.Point(0, 0);
+            this.LeftToolStripPanel.Name = "LeftToolStripPanel";
+            this.LeftToolStripPanel.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            this.LeftToolStripPanel.RowMargin = new System.Windows.Forms.Padding(4, 0, 0, 0);
+            this.LeftToolStripPanel.Size = new System.Drawing.Size(0, 0);
+            // 
+            // ContentPanel
+            // 
+            this.ContentPanel.Size = new System.Drawing.Size(150, 150);
             // 
             // Faturas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1210, 755);
+            this.ClientSize = new System.Drawing.Size(817, 633);
             this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelEmenta);
+            this.Controls.Add(this.labelDia);
+            this.Controls.Add(this.labelEstado);
             this.Controls.Add(this.monthCalendar1);
             this.Name = "Faturas";
             this.Text = "Faturas";
@@ -63,6 +140,14 @@
         #endregion
 
         private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Label labelEstado;
+        private System.Windows.Forms.Label labelDia;
+        private System.Windows.Forms.Label labelEmenta;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStripPanel BottomToolStripPanel;
+        private System.Windows.Forms.ToolStripPanel TopToolStripPanel;
+        private System.Windows.Forms.ToolStripPanel RightToolStripPanel;
+        private System.Windows.Forms.ToolStripPanel LeftToolStripPanel;
+        private System.Windows.Forms.ToolStripContentPanel ContentPanel;
     }
 }
